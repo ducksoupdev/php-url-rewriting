cac318b8323444e21e0b016485c9ee7d053b85d2
levym <ducksoupdev@gmail.com>
2017-08-31 19:17:58 +0100

    Add PHP redirect examples

---
+++ html/home.php
@@ -0,0 +1,47 @@
+<h1>Home</h1>
+
+<h3>Pictures</h3>
+<ul>
+    <li><a href="/picture/some-text/51">Picture 51</a></li>
+    <li><a href="/picture/some-text/55">Picture 55</a></li>
+    <li><a href="/picture.php">Picture (direct)</a></li>
+</ul>
+
+<h3>Albums</h3>
+<ul>
+    <li><a href="/album/album-slug-1">Album 1</a></li>
+    <li><a href="/album/album-slug-12">Album 12</a></li>
+    <li><a href="/album.php">Album (direct)</a></li>
+</ul>
+
+<h3>Categories</h3>
+<ul>
+    <li><a href="/category/category-slug-2">Category 2</a></li>
+    <li><a href="/category/category-slug-22">Category 22</a></li>
+    <li><a href="/category.php">Category (direct)</a></li>
+</ul>
+
+<h3>Pages</h3>
+<ul>
+    <li><a href="/page/about">About page</a></li>
+    <li><a href="/page/contact">Contact page</a></li>
+    <li><a href="/page.php">Page (direct)</a></li>
+</ul>
+
+<h3>Posts</h3>
+<ul>
+    <li><a href="/post-slug-1">Post 1</a></li>
+    <li><a href="/post-slug-2">Post 2</a></li>
+    <li><a href="/post.php">Post (direct)</a></li>
+</ul>
+
+<h3>Computing</h3>
+<ul>
+    <li><a href="/computing">Computing</a></li>
+    <li><a href="/computing.php">Computing (direct)</a></li>
+</ul>
+
+<h3>Home</h3>
+<ul>
+    <li><a href="/">Home</a></li>
+</ul>
